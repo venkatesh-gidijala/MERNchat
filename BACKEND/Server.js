e21d4347fb8cbba20d086e2eb49d65a31581d7d6
Venkatesh <Venkeygidijala@gmail.com>
2025-04-30 23:11:30 +0530

    database change to atlae

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -25,10 +25,10 @@ app.use('/ChatTogether/notification',NotificationRoute)
 const __dirname1 = path.resolve();  
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, 'FRONTEND', 'dist')));
+  app.use(express.static(path.join(__dirname1,'..', 'FRONTEND', 'dist')));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, 'FRONTEND', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname1,'..', 'FRONTEND', 'dist', 'index.html'));
   });
 } else {
   app.get("/", (req, res) => {
--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,14 +1,16 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config();
-const mongoose = require('mongoose')
-const mongoosconnection = async() =>{
-    try{
-        const uri = process.env.MONGO_URL || "mongodb://localhost:27017/mernchat";
-        const connect = await mongoose.connect(uri);
-        console.log("Connected to MongoDB");
-    }catch(error){
-        console.log(error)   
-        process.exit()
-     }
-}
-module.exports = mongoosconnection
+
+const mongoose = require('mongoose');
+
+const mongoosconnection = async () => {
+    try {
+        const uri = process.env.MONGO_URL;
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB Atlas");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+module.exports = mongoosconnection;
